c6a3fb49f6686ab2a547990776cc7c454249b208
OganeMakiko <m.ogane@sky-link.co.jp>
2018-04-18 12:17:08 +0900

    MasterLoaderクラス、CharacterMasterクラス作成 Master作成、Masterからの読み込み機能実装

---
+++ Undertale/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private string charaID;
+
+    private string Name { set; get; }
+    private string TalkMessage { get; set; }
+
+    private void Awake()
+    {
+        LoadCharacterMaster(charaID);
+    }
+
+    public void Talk()
+    {
+        Debug.Log(Name + " : " + TalkMessage);
+    }
+
+    private void LoadCharacterMaster(string charaID)
+    {
+        var character = MasterLoader.LoadCharacterMaster(charaID);
+        SetData(character.charaName, character.talkMessage);
+    }
+
+    private void SetData(string name, string talkMessage)
+    {
+        Name = name;
+        TalkMessage = talkMessage;
+    }
+}
---
+++ Undertale/Assets/Scripts/MasterLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class MasterLoader
+{
+    public static CharacterMaster LoadCharacterMaster(string charaID)
+    {
+        string masterPath = CharacterMaster.MasterPathHeader + "/" + charaID;
+
+        return Resources.Load(masterPath) as CharacterMaster;
+    }
+}
